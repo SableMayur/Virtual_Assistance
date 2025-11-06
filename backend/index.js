a94597d708d366318ac20eae19b7034e6f825151
Mayur Sable <mayursable077@gmail.com>
2025-11-06 11:29:32 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./config/db.js"
+import authRouter from "./routes/auth.routes.js"
+import cookieParser from "cookie-parser"
+//import cors from "cors"
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 5000
+//app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api/auth",authRouter)
+
+
+app.listen(port,()=>{
+    connectDb()
+    console.log("server started")
+})
