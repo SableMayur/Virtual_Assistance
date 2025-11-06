4226d7080ca09ce70856e9fcb3f7317b9f9249b9
Mayur Sable <mayursable077@gmail.com>
2025-11-06 17:07:16 +0530

    create a signup page and add functionality

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,16 @@
+import React, { createContext } from "react";
+
+export const userDataContext = createContext();
+
+function UserContext({ children }) {
+  const serverUrl = "http://localhost:8000";
+  const value = { serverUrl };
+
+  return (
+    <userDataContext.Provider value={value}>
+      {children}
+    </userDataContext.Provider>
+  );
+}
+
+export default UserContext;
